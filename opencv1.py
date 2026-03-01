4440324eb2ce9d77f01cdebb4c586e58d5946728
Deni Supriyatna <denisupriyatna01@gmail.com>
2026-03-01 15:37:12 +0700

    update opencv1

--- opencv1.py
+++ opencv1.py
@@ -1,4 +1,20 @@
 import cv2
-print(cv2.__version__)
-
 import matplotlib.pyplot as plt
+
+# read image as grayscale
+cb_img = cv2.imread('image/checkerboard_18x18.png', 0)
+print(cb_img)
+
+# print the size of image
+print("Image size (H, W) is : ", cb_img.shape)
+
+# print data type of image
+print("Data type image is : ", cb_img.dtype)
+
+# Display image using matplotlib
+plt.imshow(cb_img)
+plt.show()
+
+# set color map to gray scale for proper rendering
+plt.imshow(cb_img, cmap="gray")
+plt.show()
